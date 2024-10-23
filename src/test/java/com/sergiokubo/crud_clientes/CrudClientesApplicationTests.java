2023bd693fc3290d67417c47aaac87307fb26dc0
sergiokubo <sergio.t.kubo@gmail.com>
2024-10-23 18:56:35 -0300

    Projeto CRUD Clientes criado

---
+++ src/test/java/com/sergiokubo/crud_clientes/CrudClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergiokubo.crud_clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
