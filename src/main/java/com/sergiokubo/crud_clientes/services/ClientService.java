51f0d3d3679739fd36239a067ea129e8d33d9723
sergiokubo <sergio.t.kubo@gmail.com>
2024-10-26 20:28:55 -0300

    DELETE Client criado

--- src/main/java/com/sergiokubo/crud_clientes/services/ClientService.java
+++ src/main/java/com/sergiokubo/crud_clientes/services/ClientService.java
@@ -45,6 +45,11 @@ public class ClientService {
         return new ClientDTO(entity);
     }
 
+    @Transactional()
+    public void delete(Long id){
+        repository.deleteById(id);
+    }
+
     private void copyDtoToEntity(ClientDTO dto, Client entity) {
         entity.setName(dto.getName());
         entity.setCpf(dto.getCpf());
